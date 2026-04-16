7cfac3aa91159ada17e0e450048dfe0094b50779
deepanshu singh <deepanshusingh036@gmail.com>
2026-04-17 01:06:24 +0530

    fixes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:officialagreement/main.dart';
@@ -13,18 +12,12 @@ import 'package:officialagreement/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const OfficialAgreementApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Wait for the splash screen to disappear since we added a Future.delayed
+    await tester.pumpAndSettle(const Duration(seconds: 3));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our app shows Login screen text.
+    expect(find.text('Welcome Back'), findsOneWidget);
   });
 }
